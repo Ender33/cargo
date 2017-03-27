5c220806a829d267d9be4c2c30a63640d0b5aa26
Pierre <pierre.jacquet@cws-studio.com>
2017-03-27 15:14:19 +0200

    WIP

--- src/Config.php
+++ src/Config.php
@@ -8,10 +8,45 @@ namespace Cargo;
  */
 class Config
 {
-    protected $values;
+    const PARAM_KEY_SEPARATOR = '.';
 
-    public function getValue($key, $default)
+    protected static $params = [];
+
+    /**
+     * @param string $key
+     * @param mixed  $default
+     * @return string|array|int|bool
+     */
+    public static function getParam($key, $default)
     {
+        $keyParts = explode('.', $key);
+        $param = self::$params;
+        $currentPropertyPath = [];
+        foreach ($keyParts as $keyPart) {
+            if (!isset($param[$keyPart])) {
+                return $default;
+            }
+            $currentPropertyPath[] = $keyPart;
+            $param = &$param[$keyPart];
+        }
 
+        return $param;
+    }
+
+    /**
+     * @param array  $config
+     * @param string $prefix
+     * @return array
+     */
+    public static function addParamsFromArray(array $config, $prefix = '')
+    {
+        foreach ($config as $name => $configItem) {
+            $paramName = $prefix.self::PARAM_KEY_SEPARATOR.$name;
+            if (is_array($configItem)) {
+                self::$params = array_merge(self::$params, self::addParamsFromArray($configItem, $paramName));
+            } else {
+                self::$params[$paramName] = $configItem;
+            }
+        }
     }
 }
--- src/Context.php
+++ src/Context.php
@@ -2,24 +2,56 @@
 
 namespace Cargo;
 
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Parser;
+
 /**
  * Class Context
  * @package Cargo
  */
 class Context
 {
-    protected static $config;
+    protected $logger;
 
-    public function __construct($configFilePath)
+    public function __construct($env, $configFilePath)
     {
+        if (!file_exists($configFilePath)) {
+            throw new \RuntimeException('The file "'.$configFilePath.'" was not found.');
+        }
+        if (!is_readable($configFilePath)) {
+            throw new \RuntimeException('The file "'.$configFilePath.'" is not readable.');
+        }
 
-    }
+        try {
+            $parser = new Parser();
+            $mergedConfig = [];
+            $rawConfig = $parser->parse(file_get_contents($configFilePath));
+            if (!empty($rawConfig['default'])) {
+                Config::addParamsFromArray($rawConfig['default']);
+            }
+            if (!empty($rawConfig['environments'][$env])) {
+                Config::addParamsFromArray($rawConfig['environments'][$env]);
+            }
+            Config::setParams($rawParams);
+        } catch (ParseException $exception) {
+            throw new \RuntimeException('Error while parsing the file "'.$configFilePath.'".');
+        }
 
-    /**
-     * @return array
-     */
-    public static function getConfig()
-    {
-        return self::$config;
+        if (array_key_exists('magephp', $config) && is_array($config['magephp'])) {
+            $logger = null;
+            if (array_key_exists('log_dir', $config['magephp']) && file_exists($config['magephp']['log_dir']) && is_dir($config['magephp']['log_dir'])) {
+                $logfile = sprintf('%s/%s.log', $config['magephp']['log_dir'], date('Ymd_His'));
+                $config['magephp']['log_file'] = $logfile;
+
+                $logger = new Logger('magephp');
+                $logger->pushHandler(new StreamHandler($logfile));
+            }
+
+            $this->runtime->setConfiguration($config['magephp']);
+            $this->runtime->setLogger($logger);
+            return;
+        }
+
+        throw new RuntimeException(sprintf('The file "%s" does not have a valid Magallanes configuration.', $this->file));
     }
 }
